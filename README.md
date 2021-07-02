598d81c83e8de3ed3435f08ad3cf8c03dd93fcdc
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-02 19:28:24 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Level UP 3
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Level Up #3',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
