30ff5164ff32b8990bd5cca5cd006beb3b3f60ac
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-03 14:51:17 -0300

    altera métodos do UserModel

--- lib/app/shared/models/user_model.dart
+++ lib/app/shared/models/user_model.dart
@@ -9,6 +9,16 @@ class UserModel {
     required this.password,
   });
 
+  UserModel copyWith({
+    String? email,
+    String? password,
+  }) {
+    return UserModel(
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
   Map<String, dynamic> toMap() {
     return {
       'email': email,
@@ -26,4 +36,17 @@ class UserModel {
   String toJson() => json.encode(toMap());
 
   factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'UserModel(email: $email, password: $password)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserModel && other.email == email && other.password == password;
+  }
+
+  @override
+  int get hashCode => email.hashCode ^ password.hashCode;
 }
