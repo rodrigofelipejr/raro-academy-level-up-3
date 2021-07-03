839a0acea175ea36b09d24adef5fd840b962f60b
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-03 09:25:11 -0300

    adiciona widget TextFormField compartilhado

---
+++ lib/app/shared/widgets/text_form_field/text_form_field_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TextFormFieldWidget extends StatelessWidget {
+  final TextEditingController? controller;
+  final TextInputType textInputType;
+  final String label;
+  final ValueChanged<String> onChange;
+  final TextInputAction? textInputAction;
+  final bool obscureText;
+  final String? Function(String? value) validator;
+  final List<TextInputFormatter>? textInputFormatter;
+
+  const TextFormFieldWidget({
+    Key? key,
+    this.controller,
+    this.textInputType = TextInputType.text,
+    required this.label,
+    required this.validator,
+    required this.onChange,
+    this.textInputAction = TextInputAction.next,
+    this.obscureText = false,
+    this.textInputFormatter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      onChanged: onChange,
+      decoration: InputDecoration(labelText: label),
+      validator: validator,
+      textInputAction: textInputAction,
+      obscureText: obscureText,
+      keyboardType: textInputType,
+      inputFormatters: textInputFormatter,
+    );
+  }
+}
---
+++ lib/app/shared/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'text_form_field/text_form_field_widget.dart';
