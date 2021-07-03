bb4229d4a66cf5c100345cd7bb27a1367a629f85
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-03 12:36:27 -0300

    adiciona model do usuário

---
+++ lib/app/shared/models/user_model.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+class UserModel {
+  final String email;
+  final String password;
+
+  UserModel({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      email: map['email'],
+      password: map['password'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source));
+}
